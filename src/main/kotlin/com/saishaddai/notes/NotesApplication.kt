29512cb3aa1e17c69a3f30e7a9ed2e145c6c2f88
Saidel Lopez <sailopher@yahoo.com.mx>
2024-09-14 22:39:08 -0600

    Update NotesApplication.kt

--- src/main/kotlin/com/saishaddai/notes/NotesApplication.kt
+++ src/main/kotlin/com/saishaddai/notes/NotesApplication.kt
@@ -2,19 +2,9 @@ package com.saishaddai.notes
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-@RestController
-class NotesApplication {
-
-	@GetMapping
-	fun defaultMapping(): ResponseEntity<String> {
-		return ResponseEntity.ok("nothing to see here")
-	}
-}
+class NotesApplication 
 
 fun main(args: Array<String>) {
 	runApplication<NotesApplication>(*args)
